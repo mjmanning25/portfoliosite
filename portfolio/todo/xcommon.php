7bc64b32fdd2d287b38138773356e3d9999a2037
mjmanning25 <mjmanning25@gmail.com>
2019-10-31 20:13:03 +1300

    beep

--- portfolio/todo/xcommon.php
+++ portfolio/todo/xcommon.php
@@ -3,7 +3,7 @@
 // Basic security
 //if (empty($_SESSION['alevel']) or ($_SESSION['alevel'] == 0)) header("location:./index.php?i=2");
     date_default_timezone_set('Pacific/Auckland');
-	$GLOBALS['conn'] = mysqli_connect("localhost", "fred", "20Triangle", "todo");
+	$GLOBALS['conn'] = mysqli_connect("localhost", "fred", "20Triangle", "testing");
 	// error handler, remove the connect_error before release!!!
 	if(!$GLOBALS['conn']){
 		die("Connection failed: ".mysqli_connect_error());
